1206e99ffb5d5985b421d9ed19484e869b174526
scott2346413 <2346413@swansea.ac.uk>
2023-12-12 14:46:58 +0000

    seed spawner scatters seeds round map

---
+++ Assets/Scripts/seedSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seedSpawner : MonoBehaviour
+{
+    public GameObject[] seeds;
+
+    public float xBound;
+    public float zBound;
+
+    public int numberOfSeeds;
+
+    private void Start()
+    {
+        for(int i = 0; i < numberOfSeeds; i++)
+        {
+            GameObject seed = seeds[Random.Range(0, seeds.Length-1)];
+            Vector3 spawnPosition = Vector3.zero;
+            spawnPosition.x = Random.Range(-xBound, xBound);
+            spawnPosition.z = Random.Range(-zBound, zBound);
+            spawnPosition += transform.position;
+            Instantiate(seed, spawnPosition, Quaternion.identity);
+        }
+    }
+
+}
