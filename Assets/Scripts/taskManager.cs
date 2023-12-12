9626a5709b2a0846a9c887779816d2e2b3dedd58
scott2346413 <2346413@swansea.ac.uk>
2023-12-12 14:18:05 +0000

    Added tasks (scriptable object). Tracked by task manager

--- Assets/Scripts/taskManager.cs
+++ Assets/Scripts/taskManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class taskManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public task[] tasks;
+    Dictionary<task, GameObject[]> taskObjects = new Dictionary<task, GameObject[]>();
+
+    public TextMeshProUGUI tasksText;
+
+    private void Start()
     {
-        
+        foreach (task task in tasks)
+        {
+            taskObjects[task] = GameObject.FindGameObjectsWithTag(task.objectsTag);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        string taskProgressText = "";
+
+        foreach (task task in tasks)
+        {
+            float progress = 0;
+            GameObject[] objects = taskObjects[task];
+
+            foreach (GameObject taskObject in objects)
+            {
+                switch (task.type)
+                {
+                    case task.TaskType.DestroyObject:
+                        if (taskObject == null) progress++;
+                        break;
+                    case task.TaskType.ActivateObject:
+                        if (taskObject.activeInHierarchy == true) progress++;
+                        break;
+                }
+            }
+
+            string taskText = task.name + " [" + progress + "/" + objects.Length + "] \n";
+            taskProgressText += taskText;
+        }
+
+        tasksText.text = taskProgressText;
     }
 }
