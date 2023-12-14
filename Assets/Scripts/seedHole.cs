13ddb9e7842b24721951e2943688795b729ffe25
scott2346413 <2346413@swansea.ac.uk>
2023-12-14 12:43:19 +0000

    seed hole becomes lighter colour when seed planted

--- Assets/Scripts/seedHole.cs
+++ Assets/Scripts/seedHole.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class seedHole : MonoBehaviour
 {
     bool isEmpty = true;
+    MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag.Equals("Seed") && isEmpty)
         {
+            Color colour = meshRenderer.material.color;
+            colour.g += 0.1f;
+            colour.b += 0.1f;
+            colour.r += 0.1f;
+            meshRenderer.material.color = colour;
             Destroy(collision.gameObject);
             isEmpty = false;
         }
